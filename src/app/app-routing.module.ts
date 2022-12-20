5a3486131ed7ef1dbfb50e0aa0c926f1000d62af
Alonso Paucar <iaquintanapa@gmail.com>
2022-12-19 19:22:27 -0500

    Muchas cosas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'template'
+  },
+  {
+    path: 'reactivate'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
