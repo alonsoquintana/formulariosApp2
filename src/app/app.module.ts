bc3dd1d29742bcad7bc14205132dc9432d011cbf
Alonso Paucar <iaquintanapa@gmail.com>
2022-12-20 19:44:14 -0500

    FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { BasicosComponent } from './template/basicos/basicos.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BasicosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
